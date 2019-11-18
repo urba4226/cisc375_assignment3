41af63e4fae2340ca3237ab903851bb12951339a
urba4226 <37028174+urba4226@users.noreply.github.com>
2019-11-18 16:43:23 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 // open usenergy.sqlite3 database
-var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
+var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.log('Error opening ' + db_filename);
     }
@@ -282,157 +282,54 @@ app.get('/incidents', (req, res) =>
             {
                 res.type('json').send(response);
             }   //else
-        }
-    });
-});
-
-//GET request handler for incidents
-/*
-app.get('/incidents', (req, res) => 
-{
-	let query = "SELECT * FROM Incidents ORDER BY date_time desc";
-    db.all(query, (err, rows) =>
-    {
-        if (err)
-        {
-            let msg = "Error: could not retrieve data from database";
-            Write404Error(res, msg);
-        }   //if
-        else
-        {
-        	//Build JSON object:
-        	let response = {};
-			for (let i=0; i<10; i++)
-        	{
-        		let test = true;
-        		let datetime = rows[i].date_time.split("T");
-        		let date = datetime[0];
-        		let time = datetime[1];
-        		let temp = {};
-        		temp['I' + rows[i].case_number] = 	{
-    													'date': date,
-    													'time': time,
-    													'code': rows[i].code,
-    													'incident': rows[i].incident,
-    													'police_grid': rows[i].police_grid,
-    													'neighborhood_number': rows[i].neighborhood_number,
-    													'block': rows[i].block
-        											};
-                //Handler for id:
-        		if (req.query.id)
-        		{
-					let ids = req.query.id.split(",");
-    				if(!(ids.includes(temp['I' + rows[i].case_number].neighborhood_number.toString(10))))
-    				{
-    					test = false;
-    				}	//if
-        		}	//if
-                //Handler for code:
-                if (req.query.code)
-                {
-                    let codes = req.query.code.split(",");
-                    if (!(codes.includes(rows[i].code.toString(10))))
-                    {
-                        test = false;
-                    }   //if
-                }   //if
-                //Handler for grid:
-                if (req.query.grid)
-                {
-                    let grids = req.query.grid.split(",");
-                    if (!(grids.includes(rows[i].police_grid.toString(10))))
-                    {
-                        test = false;
-                    }   //if
-                }   //if
-                //Handler for start_date:
-                if (req.query.start_date)
-                {
-
-                }   //if
-                //Handler for end_date:
-                {
-
-                }   //if
-        		if (test)
-        		{
-        			response['I' + rows[i].case_number] = 	{
-		    													'date': date,
-		    													'time': time,
-		    													'code': rows[i].code,
-		    													'incident': rows[i].incident,
-		    													'police_grid': rows[i].police_grid,
-		    													'neighborhood_number': rows[i].neighborhood_number,
-		    													'block': rows[i].block
-	        												};
-        		}	//if
-        	}	//for
-        	
-        	//Handler for format option:
-			if (req.query.format === 'json')
-			{
-				res.type('json').send(response);
-			}	//if
-			else if (req.query.format === 'xml')
-			{
-				let xml = js2xmlparser.parse("incidents", response);
-				res.type('xml').send(xml);
-			}	//else if
-			else
-			{
-				res.type('json').send(response);
-			}	//else
-        }	//else
-    });	//db.all
-});	//app.get
+        }   //else
+    }); //db.all
+}); //app.get
 
 //PUT request handler for new-incident
 app.put('/new-incident', (req, res) =>
 {
-	var new_user = 
-	{
-		id: parseInt(req.body.id),
-		name: req.body.name,
-		email: req.body.email
-	};	//new_user
+    if(!(req.body.case_number && req.body.date && req.body.time && req.body.code && req.body.incident && 
+            req.body.police_grid && req.body.neighborhood_number && req.body.block))
+    {
+        res.status(500).send('Error: Missing parameter. Required parameters: case_number, date, time, code, incident, police_grid, neighborhood_number, block');
+    }   //if
 	var has_id = false;
-	for (let i = 0; i < users.users.length; i++)
-	{
-		if (users.users[i].id === new_user.id)
-		{
-			has_id = true;
-			break;
-		}	//if
-	}	//for
-	if (has_id)
-	{
-		res.status(500).send('Error: case number already exists');
-	}	//if
-	else
-	{
-		users.users.push(new_user);
-		fs.writeFile(users_filename,JSON.stringify(users, null, 4), (err) =>
-		{
-			res.status(200).send('Success!');
-		});	//writeFile
-	}	//else
+    let query = "Select * FROM incidents WHERE case_number = ?";
+	db.all(query, [req.body.case_number], (err, rows) =>
+    {
+        if (rows.length > 0)
+        {
+            has_id = true;
+        }   //if
+        if (has_id)
+        {
+            res.status(500).send('Error: case number already exists');
+        }   //if
+        else
+        {
+            let date_time = req.body.date + "T" + req.body.time;
+            let parameters = [req.body.case_number, date_time, req.body.code, req.body.incident, 
+                                req.body.police_grid, req.body.neighborhood_number, req.body.block];
+            let query = "";
+            query = "INSERT INTO Incidents (case_number, date_time, code, incident, police_grid, neighborhood_number, block) ";
+            query = query + " VALUES(?, ?, ?, ?, ?, ?, ?)";
+            db.run(query, parameters, (err, rows) =>
+            {
+                if (err)
+                {
+                    console.log(err);
+                    res.status(500).send('Error adding new incident');
+                }   //if
+                else
+                {
+                    res.status(200).send('Successfully added new incident!');
+                }   //else
+            }); //db.run
+        }   //else
+    }); //db.get
+	
 });	//app.put
-*/
-
-function Write404Error(res, msg) 
-{
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.write(msg);
-    res.end();
-}	//Write404Error
-
-function WriteHtml(res, html) 
-{
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(html);
-    res.end();
-}	//WriteHtml
-
 
 var server = app.listen(port);
 console.log('Now listening on port ' + port);
