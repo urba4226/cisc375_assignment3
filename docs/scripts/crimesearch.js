fc8c4a5e007fe3d9ad89240951aa058ccaebcb00
urba4226 <37028174+urba4226@users.noreply.github.com>
2019-12-11 16:17:18 -0600

    Add files via upload

--- docs/scripts/crimesearch.js
+++ docs/scripts/crimesearch.js
@@ -1,57 +1,52 @@
 var app;
-var auth_data = {};
+var api;
 
-function Init()
+function Prompt() {
+    $("#dialog-form").dialog({
+        autoOpen: true,
+        modal: true,
+        width: "360px",
+        buttons: {
+            "Ok": function() {
+                var prompt_input = $("#prompt_input");
+                Init(prompt_input.val());
+                $(this).dialog("close");
+            },  //ok
+            "Cancel": function() {
+                $(this).dialog("close");
+            }   //cancel
+        }   //buttons
+    }); //dialog
+}   //Prompt
+
+function Init(crime_api_url)
 {
-    /*
-    // Spotify authentication
-    if (!window.location.hash || window.location.hash.length < 15 || window.location.hash.substring(0, 14) !== "#access_token=")
-    {
-        let spotify_client_id = "01d2501b06fc4d4f8e27eaaead009c0e";
-        
-        window.location = "https://accounts.spotify.com/authorize?client_id=" + spotify_client_id + 
-                          "&response_type=token&redirect_uri=" + window.location.origin + "/index.html" +
-                          "&state=1234";
-    }   //if
-
-    // Actual app, once authenticated
-    let i, key_val;
-    let auth_array = window.location.hash.substring(1).split("&");
-    for (i = 0; i < auth_array.length; i++)
-    {
-        key_val = auth_array[i].split("=");
-        if (key_val.length === 2)
-        {
-            auth_data[key_val[0]] = key_val[1];
-        }   //if
-    }   //for
-    */
-
-    loadNeighborhoods();
+    api = crime_api_url;
+    LoadNeighborhoods(crime_api_url);
+    LoadIncidents(crime_api_url);
+    CrimeSearch(crime_api_url);
 
     app = new Vue({
         el: "#app",
         data: {
+            //api_url: "",
             search_results: [],
             neighborhoods: [],
-            selectedNeighborhoods: []
+            incidents: [],
+            selectedNeighborhoods: [],
+            selectedIncidents: [],
+            start_date: "",
+            end_date: "",
+            start_time: "",
+            end_time: ""
         }  //data
-        /*
-        computed: {
-            input_placeholder: function() {
-                if (this.spotify_type[0] === "a")
-                    return "Search for an " + this.spotify_type;
-                return "Search for a " + this.spotify_type;
-            }   //input_placeholder
-        }   //computed
-        */
     }); //Vue
 }   //init
 
-function loadNeighborhoods()
+function LoadNeighborhoods(api_url)
 {
     let request = {
-        url: "http://cisc-dean.stthomas.edu:8022/neighborhoods",
+        url: api_url + "/neighborhoods",
         dataType: "json",
         success: NeighborhoodData
     };  //request
@@ -62,15 +57,117 @@ function NeighborhoodData(data)
 {
     app.neighborhoods = data;
     console.log(data);
-}   //CrimeData
+}   //NeighborhoodData
 
-function CrimeSearch(event)
+function LoadIncidents(api_url)
 {
     let request = {
-        url: "http://cisc-dean.stthomas.edu:8022/incidents?start_date=2019-10-01&end_date=2019-10-31",
+        url: api_url + "/codes",
+        dataType: "json",
+        success: IncidentData
+    };  //request
+    $.ajax(request);
+}   //loadIncidents
+
+function IncidentData(data)
+{
+    app.incidents = data;
+    console.log(data);
+}   //IncidentData
+
+function CrimeSearch(api_url)
+{
+    let request = {
+        url: api_url + "/incidents?start_date=2019-10-01&end_date=2019-10-31",
         dataType: "json",
         success: CrimeData
     };  //request
+    $.ajax(request);
+}   //CrimeSearch
+
+
+function FilteredSearch(neighborhoods, incidents, start_date, end_date)
+{
+    let first = true;
+    let neighborhoods_filter = "";
+    let incidents_filter = "";
+    let start_date_filter = "";
+    let end_date_filter = "";
+
+    //Handler for if neighborhoods is filtered:
+    if (neighborhoods.length > 0)
+    {
+        if (first)
+        {
+            first = false;
+            neighborhoods_filter = "?id=";
+        }   //if
+        else
+        {
+            neighborhoods_filter = "&id=";
+        }
+        neighborhoods_filter = neighborhoods_filter + neighborhoods[0];
+        for (let i=1; i<neighborhoods.length; i++)
+        {
+            neighborhoods_filter = neighborhoods_filter + "," + neighborhoods[i];
+        }   //for
+    }   //if
+
+    //Handler for if incidents is filtered:
+    if (incidents.length > 0)
+    {
+        if (first)
+        {
+            first = false;
+            incidents_filter = "?code=";
+        }   //if
+        else
+        {
+            incidents_filter = "&code=";
+        }   //else
+        incidents_filter = incidents_filter + incidents[0];
+        for (let i=1; i<incidents.length; i++)
+        {
+            incidents_filter = incidents_filter + "," + incidents[i];
+        }   //for
+    }   //if
+
+    //Handler for if start_date is set:
+    if (start_date.length > 0)
+    {
+        if (first)
+        {
+            first = false;
+            start_date_filter = "?start_date=";
+        }   //if
+        else
+        {
+            start_date_filter = "&start_date=";
+        }   //else
+        start_date_filter = start_date_filter + start_date;
+    }   //if
+
+    //Handler for if end_date is set:
+    if (end_date.length > 0)
+    {
+        if (first)
+        {
+            first = false;
+            end_date_filter = "?end_date=";
+        }   //if
+        else
+        {
+            end_date_filter = "&end_date=";
+        }   //else
+        end_date_filter = end_date_filter + end_date;
+    }   //if
+
+    let request = {
+        url: api + "/incidents" + neighborhoods_filter + incidents_filter + start_date_filter + end_date_filter,
+        dataType: "json",
+        success: CrimeData
+    };  //request
+    console.log(request.url);
     $.ajax(request);
 }   //CrimeSearch
 
@@ -79,3 +176,8 @@ function CrimeData(data)
     app.search_results = data;
     console.log(data);
 }   //CrimeData
+
+function AddCrime(case_num)
+{
+    console.log("case_num: " + case_num);
+}   //AddCrime
